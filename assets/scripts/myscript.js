9cdcce5b8d9a25d86524b6c3493a38dcada728d7
4217A <51804561+4217A@users.noreply.github.com>
2020-10-18 16:26:33 -0500

    Add files via upload

--- assets/scripts/myscript.js
+++ assets/scripts/myscript.js
@@ -4,13 +4,18 @@ LIGHT_BKGND = '#f7f7f7'; //#f7f7f7
 
 const light = document.getElementById('customSwitch1');
 const dark = document.getElementById('customSwitch2');
+const myHeight = window.innerHeight;
+const firstRow = document.getElementById('my_first_row').clientHeight;
 
 //initialize
 light.checked = true;
 dark.checked = false;
-//console.log(getComputedStyle(document.documentElement).getPropertyValue('--mycolor'));
 document.documentElement.style.setProperty('--mycolor', LIGHT_TEXT);
 document.documentElement.style.setProperty('--mybackgroundcolor', LIGHT_BKGND);
+document.documentElement.style.setProperty('--myheight', (((myHeight-firstRow)/2)*.7 +'px'));
+//console.log(myHeight);
+//console.log((myHeight/2)*.8 +'px');
+//console.log(((myHeight-firstRow)/2)*.7 +'px');
 
 const setLight = () => {
     document.documentElement.style.setProperty('--mycolor', LIGHT_TEXT);
